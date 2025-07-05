948a91c2c66d9aa5dbf2d8defb808728705926b5
Kishan Tripathi <kishantiwari231@gmail.com>
2025-07-05 14:16:37 +0530

    commited

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+function App(){
+  return (
+    <>
+    <h1>List </h1>
+    <ul>
+      <li>HTML</li>
+      <li>JAvascript</li>
+      <li>React</li>
+    </ul>
+    
+    
+    </>
+    
+  )
+}
+export default App;
